f6922d7fd5fd7c93ef010bf7bad804bfa51e32f6
Sadaf244 <77284689+Sadaf244@users.noreply.github.com>
2021-11-03 14:57:10 +0530

    Add files via upload

---
+++ user_app/tests.py
@@ -0,0 +1,36 @@
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from rest_framework.authtoken.models import Token
+from django.contrib.auth.models import User
+
+class RegisterTestCase(APITestCase):
+
+    def test_register(self):
+        data={
+            "username":"testcase",
+            "email":"test@example.com",
+            "password":"testpass",
+            "password2":"testpass"    
+        }
+        response = self.client.post(reverse('registration'), data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+class LoginLogoutTestCase(APITestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user(username="example",password="testpass@123")
+    
+    def test_login(self):
+        data={
+            "username":"example",
+            "password":"testpass@123"
+        }
+        response = self.client.post(reverse('login'), data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+    def test_logout(self):
+        self.token=Token.objects.get(user__username="example")
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
+        response = self.client.post(reverse('logout'))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
